c426f35844856416d84f1ec4141f21f0f297a9ed
deepshikha <deepshikhaverma44@gmail.com>
2023-04-29 15:22:37 +0530

    fixed idx issue

--- searching/AthMagicalNumber.py
+++ searching/AthMagicalNumber.py
@@ -2,13 +2,13 @@ class solution:
     def solve(self,A,B,C):
         s = min(B,C)
         e = A*s
-        lcm = (B*C)/self.hcf(B,C)
+        lcm = (B*C)//self.hcf(B,C)
         result = 0
         d= (10**9)+7
         while(s<=e):
             mid = (s+e)//2
-            idx = int(mid/B+mid/C-mid/lcm)
-            print('idx=',idx, ' A=',A, ' S=',s, ' E=',e, ' mid=',mid)
+            idx = int(mid//B + mid//C - mid//lcm)
+            print('idx=',idx, ' A=',A, ' S=',s, ' E=',e, ' mid=',mid, ' lcm=',lcm)
             if(idx>=A):
                 result = mid
                 e = mid-1
@@ -22,8 +22,8 @@ class solution:
         return self.hcf(B, A%B)
 
 
-A = 4
-B = 2
-C = 3
+A = 19
+B = 11
+C = 13
 obj = solution()
 print(obj.solve(A,B,C))
