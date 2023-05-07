e5c929ea656244e289c02aeca73190519b88b66f
deepshikha <deepshikhaverma44@gmail.com>
2023-05-07 12:00:38 +0530

    Count Sorting

--- sorting/countSort.py
+++ sorting/countSort.py
@@ -3,17 +3,17 @@ class sorting:
         F = [0]*(10**5)
         
         for ch in A:
-            F[ch-1] += 1
+            F[ch] += 1
         
         idx = 0
         for i in range(10**5):
             cnt = F[i]
             if cnt:
                 for j in range(cnt):
-                    A[idx] = i+1
+                    A[idx] = i
                     idx += 1
         return A
 
-A = [4, 2, 1, 3]
+A = [9,6,4,11,2,4,1,8]
 obj = sorting()
 print(obj.countSort(A))
