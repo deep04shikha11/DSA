84e0bc68e9166157932dc0fb5b0724e34e4d6783
deepshikha <deepshikhaverma44@gmail.com>
2023-05-12 10:02:27 +0530

    optimized solution

--- TwoPointer/PairsSum.py
+++ TwoPointer/PairsSum.py
@@ -2,19 +2,38 @@ class PairOfSum:
     def solve(self,A,B):
         n = len(A)
         p1 = 0
-        p2 = n-1
-        cnt = 0
-        # print('p1=',p1,'p2=',p2,'B=',B)
-        while(p1<p2):
-            print('p1=',p1,'p2=',p2,'B=',B)
-            if (A[p1]+A[p2]==B):
-                cnt += 1
+        p2 = n - 1
+        mod = pow(10, 9) + 7
+        ans = 0
+        while p1 < p2:
+            summ = A[p1] + A[p2]
+            if summ < B:
                 p1 += 1
-            if (A[p1]+A[p2])<B:
-                p1 += 1
-            if (A[p1]+A[p2]>B):
+            elif summ > B:
                 p2 -= 1
-        return cnt
+            else:
+                if A[p1] == A[p2]:
+                    # this is a scenario when we encounter duplicates
+                    # thus, there are no other elements between p1 and p2 now
+                    x = p2 - p1 + 1
+                    ans += ((x * (x - 1)) // 2) % mod  # xC2
+                    break
+                else:
+                    dup1 = 1
+                    p1 += 1
+                    while A[p1] == A[p1 - 1]:
+                        p1 += 1
+                        dup1 += 1
+
+                    dup2 = 1
+                    p2 -= 1
+                    while A[p2] == A[p2 + 1]:
+                        p2 -= 1
+                        dup2 += 1
+
+                    ans += (dup1 * dup2) % mod
+
+        return ans % mod
 
 A = [1, 1, 1]
 B = 2
