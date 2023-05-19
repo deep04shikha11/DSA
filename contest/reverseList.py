4168bc4137935c92bcc800c4c5532e4746ac39bb
deepshikha <deepshikhaverma44@gmail.com>
2023-05-19 09:34:19 +0530

    reverse list

--- contest/reverseList.py
+++ contest/reverseList.py
@@ -4,4 +4,13 @@ class solution:
         self.val = x
         self.next = None
     def solve(self,A):
+        current = A
+        previous=None
+        while(current!=None):
+            temp=current.next
+            current.next=previous
+            previous=current
+            current=temp
+        A=previous
+        return A
         
